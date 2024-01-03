77ff1d1461d97c737abae14b89c5e4e3e68a7864
bmoxey <bmoxey@gmail.com>
2024-01-03 11:48:10 +1100

    Initial Commit

---
+++ Hockey TAS Data/Hockey_TAS_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hockey_TAS_DataApp.swift
+//  Hockey TAS Data
+//
+//  Created by Brett Moxey on 3/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hockey_TAS_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
